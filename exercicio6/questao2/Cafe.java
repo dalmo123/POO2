38779f738be98cced6700058c132892454170d55
dalmo123 <66819868+dalmo123@users.noreply.github.com>
2023-10-08 20:50:07 -0300

    Update Cafe.java

--- exercicio6/questao2/Cafe.java
+++ exercicio6/questao2/Cafe.java
@@ -1,1 +1,13 @@
 
+
+public class Cafe extends PreparadorBebida {
+	@Override
+    protected void prepararBebidaBase() {
+        System.out.println("Fazendo cafe");
+    }
+
+    @Override
+    protected void adicionarCondimentos() {
+        System.out.println("Adicionando acucar e mexendo");
+    }
+}
